4b4111c5aa46a16ff264a54166982a3d5822b3e8
yannislechevere <douglasse.ylc@gmail.com>
2025-01-20 13:00:42 +0100

    Revert "Merge branch 'main' of https://github.com/Arthurchvl/SAE-1.05"
    This reverts commit 0e523d16e0e7c866ff77ebe9529ec52b7fbd4a18, reversing
    changes made to 855114883aea437274c33d1e4b6721fe6344227b.

--- code/codeFinal/carousel.js
+++ code/codeFinal/carousel.js
@@ -1,34 +1,18 @@
-<section id="ouverture">
-            <h2>Cérémonie D'ouverture</h2>
-                <div class="carousel">
-                    <div class="carousel-images" id="carousel-images">
-                        <img src="images/cérémonie_ouverture_jo.png" alt="Cérémonie d'ouverture">
-                        <img src="images/zidane.png" alt="Image 2">
-                        <img src="images/ceremonie_ouverture2.png" alt="Image 3">
-                    </div>
-                    <div class="carousel-buttons">
-                        <button onclick="prevSlide()">&#8249;</button>
-                        <button onclick="nextSlide()">&#8250;</button>
-                    </div>
-                </div>
-                <script>
-                    let currentIndex = 0;
-                    const images = document.getElementById('carousel-images');
-                    const totalImages = images.children.length;
-                    
-                    function updateCarousel() {
-                    const offset = -currentIndex * 100;
-                    images.style.transform = `translateX(${offset}%)`;
-                    }
-                    
-                    function nextSlide() {
-                    currentIndex = (currentIndex + 1) % totalImages;
-                    updateCarousel();
-                    }
-                    
-                    function prevSlide() {
-                    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-                    updateCarousel();
-                    }
-                </script> 
-        </section>
+let currentIndex = 0;
+const images = document.getElementById('carousel-images');
+const totalImages = images.children.length;
+
+function updateCarousel() {
+const offset = -currentIndex * 100;
+images.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+currentIndex = (currentIndex + 1) % totalImages;
+updateCarousel();
+}
+
+function prevSlide() {
+currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+updateCarousel();
+}
